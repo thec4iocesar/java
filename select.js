6d2efb112f8de252c5ef4143fd8be620bb1c22a2
thec4iocesar <53881700+thec4iocesar@users.noreply.github.com>
2019-12-30 18:24:47 -0300

    Add files via upload

---
+++ select.js
@@ -0,0 +1,44 @@
+import java.sql.*;
+
+public class JavaMysqlSelectExample
+{
+
+  public static void main(String[] args)
+  {
+    try
+    {
+
+      String myDriver = "org.gjt.mm.mysql.Driver";
+      String myUrl = "jdbc:mysql://localhost/test";
+      Class.forName(myDriver);
+      Connection conn = DriverManager.getConnection(myUrl, "root", "");
+
+	  
+      String query = "SELECT * FROM *";
+
+      Statement st = conn.createStatement();
+      
+	  ResultSet rs = st.executeQuery(query);
+      
+      // iterate through the java resultset
+      while (rs.next())
+      {
+        int id = rs.getInt("id");
+        String firstName = rs.getString("first_name");
+        String lastName = rs.getString("last_name");
+        Date dateCreated = rs.getDate("date_created");
+        boolean isAdmin = rs.getBoolean("is_admin");
+        int numPoints = rs.getInt("num_points");
+        
+        // print the results
+        System.out.format("%s, %s, %s, %s, %s, %s\n", id, firstName, lastName, dateCreated, isAdmin, numPoints);
+      }
+      st.close();
+    }
+    catch (Exception e)
+    {
+      System.err.println("Got an exception! ");
+      System.err.println(e.getMessage());
+    }
+  }
+}
